f6542f9106ccfc43f306a8f4cd8d4150640f77ea
javier008l <javier.1701813899@ucaldas.edu.co>
2023-11-17 12:18:28 -0500

    creacion de clase memoria y bus sistema

--- Memoria.py
+++ Memoria.py
@@ -2,4 +2,13 @@ class Memoria:
     def __init__(self, instrucciones, dato):
         self.intrucciones = instrucciones
         self.dato = dato
-    
+
+    def leer_archivo(nombre_archivo):
+        try:
+            with open(nombre_archivo, 'r') as archivo:
+                contenido = archivo.read()
+            return contenido
+        except FileNotFoundError:
+            print(f"El archivo {nombre_archivo} no existe.")
+            return None
+    url = leer_archivo("nombre.txt")
